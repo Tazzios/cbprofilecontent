801a8e94375f1f05306b64df8f737a201a0ea271
Tazzios <23451105+Tazzios@users.noreply.github.com>
2022-09-24 18:44:31 +0200

    Update intext.php
    show real name

--- tmpl/intext.php
+++ tmpl/intext.php
@@ -15,7 +15,7 @@ defined('_JEXEC') or die;
 
 
 <a href=index.php?option=com_comprofiler&task=userProfile&user=<?php echo $userprofile['id']; ?>>
-	 <?php echo $userprofile['username']; ?>
+	 <?php echo $userprofile['name']; ?>
 	 <img src="<?php echo JURI::base()."/images/comprofiler/".$userprofile['avatar']; ?>" alt='<?php echo $userprofile['username']; ?>' title='<?php echo $userprofile['username']; ?> ' style='float: ghh; margin-left:0px; margin-top:5px; margin-bottom:5px; margin-right:5px;  width:<?php echo $imagesize['width']; ?>px; height:<?php echo $imagesize['height']; ?>px;'>
 </a>
 						
