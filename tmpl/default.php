2b68c40e450a3fe0387809e2665de0712e7f6094
Tazzios <23451105+Tazzios@users.noreply.github.com>
2022-09-26 14:35:03 +0200

    obey image ratio
    #1

--- tmpl/default.php
+++ tmpl/default.php
@@ -13,7 +13,7 @@ defined('_JEXEC') or die;
 
 
 <a href=index.php?option=com_comprofiler&task=userProfile&user=<?php echo $userprofile['id']; ?>>
-	<img src="<?php echo JURI::base()."/images/comprofiler/".$userprofile['avatar']; ?>" alt='<?php echo $userprofile['name']; ?>' title='<?php echo $userprofile['name']; ?> ' style='float: ghh; margin-left:0px; margin-top:5px; margin-bottom:5px; margin-right:5px;  width:<?php echo $imagesize['width']; ?>px; height:<?php echo $imagesize['height']; ?>px;'>
+	<img src="<?php echo JURI::base()."/images/comprofiler/".$userprofile['avatar']; ?>" alt='<?php echo $userprofile['name']; ?>' title='<?php echo $userprofile['name']; ?> ' width="<?php echo $imagesize['width']; ?>" height="<?php echo $imagesize['height']; ?>" style='margin-left:0px; margin-top:5px; margin-bottom:5px; margin-right:5px;'>
 </a>
 						
 
