a68a55c72310d1ce6505bf8ed87f345e6cffe551
Tazzios <23451105+Tazzios@users.noreply.github.com>
2022-11-01 15:20:02 +0100

    Update div-avatar.php

--- tmpl/div-avatar.php
+++ tmpl/div-avatar.php
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 ?>
 
 
-<Div class="cbprofile-userprofile" style="display: inline-block;overflow-wrap: break-word;padding: 5px;width: <?php echo $imagesize['width']; ?>px;float: <?php echo $float; ?>;">
+<Div class="cbprofile-userprofile" style="vertical-align: top;display: inline-block;overflow-wrap: break-word;padding: 5px;width: <?php echo $imagesize['width']; ?>px;float: <?php echo $float; ?>;">
 <a href=<?php echo JURI::base();?>index.php?option=com_comprofiler&task=userProfile&user=<?php echo $userprofile['id']; ?>>
 	 <img src="<?php echo JURI::base()."/images/comprofiler/".$userprofile['avatar']; ?>" alt='<?php echo $userprofile['name']; ?>' title='<?php echo $userprofile['name']; ?> ' class="cbprofile-avatar" width="<?php echo $imagesize['width']; ?>" height="<?php echo $imagesize['height']; ?>">
 	</a>
