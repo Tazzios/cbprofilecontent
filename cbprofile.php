d9a4dffbe908447d9619998e3666e303105046cc
Tazzios <23451105+Tazzios@users.noreply.github.com>
2023-04-14 16:58:24 +0200

    release to autoupdate
    fix a bad change en enable autoupdate

--- cbprofile.php
+++ cbprofile.php
@@ -241,10 +241,8 @@ class PlgContentcbprofile extends JPlugin
 					}
 					
 					// intext block start
-					if (isset($tagparameters['intext']) ) { 
-						if ($tagparameters['intext']<>'true') {
+					if (!isset($tagparameters['intext']) and $tagparameters['intext']<>'true'  ) {
 							$output .=	'<div class="cbprofile-block" style="'. $style.'">';
-						}
 					} 
 					
 					// creat html for each profile
@@ -256,10 +254,8 @@ class PlgContentcbprofile extends JPlugin
 					}
 					
 					// intext block end
-					if (isset($tagparameters['intext'])) { 
-						if ($tagparameters['intext']<>'true') {
+					if (!isset($tagparameters['intext']) and $tagparameters['intext']<>'true'  ) {
 							$output .=	'</div>';
-						}
 					}
 					
 							
